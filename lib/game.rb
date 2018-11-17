22c91771665a411bbce12e241d49dc491caa48e2
mateuszstacel <mateusz.stacel92@gmail.com>
2018-11-17 21:02:17 +0000

    updated game rps

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,16 @@ class Game
 
 def initialize(player_1, player_2)
 @players = [player_1, player_2]
+@score_player_1 = 0
+@score_player_2 = 0
+end
+
+def score_player_1
+  @score_player_1
+end
+
+def score_player_2
+  @score_player_2
 end
 
 def player_1
@@ -13,24 +23,91 @@ def player_2
 end
 
 def result(player_1_input, player_2_input)
+
   if (player_1_input == "rock" && player_2_input == "scissors")
+    @score_player_1 += 1
     @players.first
 
   elsif (player_1_input == "paper" && player_2_input == "rock")
+    @score_player_1 += 1
     @players.first
 
   elsif (player_1_input == "scissors" && player_2_input == "paper")
+    @score_player_1 += 1
     @players.first
 
+  # elsif (player_1_input == "rock" && player_2_input == "lizard")
+  #   @score_player_1 += 1
+  #   @players.first
+  #
+  # elsif (player_1_input == "lizard" && player_2_input == "paper")
+  #   @score_player_1 += 1
+  #   @players.first
+  #
+  # elsif (player_1_input == "lizard" && player_2_input == "spock")
+  #   @score_player_1 += 1
+  #   @players.first
+  #
+  # elsif (player_1_input == "spock" && player_2_input == "rock")
+  #   @score_player_1 += 1
+  #   @players.first
+  #
+  # elsif (player_1_input == "spock" && player_2_input == "paper")
+  #   @score_player_1 += 1
+  #   @players.first
+  #
+  # elsif (player_1_input == "spock" && player_2_input == "scissors")
+  #   @score_player_1 += 1
+  #   @players.first
+  #
+  # elsif (player_1_input == "scissors" && player_2_input == "lizard")
+  #   @score_player_1 += 1
+  #   @players.first
+
   elsif (player_2_input == "rock" && player_1_input == "scissors")
+    @score_player_2 += 1
     @players.last
 
   elsif (player_2_input == "paper" && player_1_input == "rock")
+    @score_player_2 += 1
     @players.last
 
   elsif (player_2_input == "scissors" && player_1_input == "paper")
+    @score_player_2 += 1
     @players.last
+
+  # elsif (player_1_input == "lizard" && player_2_input == "rock")
+  #   @score_player_2 += 1
+  #   @players.last
+  #
+  # elsif (player_1_input == "paper" && player_2_input == "lizard")
+  #   @score_player_2 += 1
+  #   @players.last
+  #
+  # elsif (player_1_input == "spock" && player_2_input == "lizard")
+  #   @score_player_2 += 1
+  #   @players.last
+  #
+  # elsif (player_1_input == "rock" && player_2_input == "spock")
+  #   @score_player_2 += 1
+  #   @players.last
+  #
+  # elsif (player_1_input == "paper" && player_2_input == "spock")
+  #   @score_player_2 += 1
+  #   @players.last
+  #
+  # elsif (player_1_input == "scissors" && player_2_input == "spock")
+  #   @score_player_2 += 1
+  #   @players.last
+  #
+  # elsif (player_1_input == "lizard" && player_2_input == "scissors")
+  #   @score_player_2 += 1
+  #   @players.last
+
+
  end
 end
+
+
 
 end
